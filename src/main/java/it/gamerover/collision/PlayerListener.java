b1c59303dbab1c9854dbd7858bb92d61695bfa83
Gabriele C <sgdc3.mail@gmail.com>
2016-08-23 14:46:45 +0200

    Project cleanup 1/2

---
+++ src/main/java/it/gamerover/collision/PlayerListener.java
@@ -0,0 +1,28 @@
+package it.gamerover.collision;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerListener implements Listener {
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerJoinEvent(PlayerJoinEvent event) {
+		
+		Player player = event.getPlayer();
+		TeamUtils.sendTeamPacket(player);
+		
+	}
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		
+		Player player = event.getPlayer();
+		TeamUtils.getSecurePlayers().remove(player);
+		
+	}
+	
+}
---
+++ src/main/java/it/gamerover/collision/TeamUtils.java
@@ -0,0 +1,74 @@
+package it.gamerover.collision;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+
+import it.gamerover.collision.tinyprotocol.Reflection;
+import it.gamerover.collision.tinyprotocol.TinyProtocol;
+
+public class TeamUtils {
+
+	private static ArrayList<Player> securePlayers = new ArrayList<Player>();
+	
+	private static Class<?> packetTeamClass = null;
+	private static Field nameField = null;
+	private static Field modeField = null;
+	private static Field collisionRuleField = null;
+	private static Field playersField = null;
+
+	static {
+
+		try {
+
+			packetTeamClass = TinyProtocol.PACKET_SCOREBOARD_TEAM;
+
+			nameField = Reflection.getField(packetTeamClass, "a");
+			modeField = Reflection.getField(packetTeamClass, "i");
+			collisionRuleField = Reflection.getField(packetTeamClass, "f");
+			playersField = Reflection.getField(packetTeamClass, "h");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	public synchronized static void sendTeamPacket(Player player) {
+
+		try {
+
+			Object packetTeamObject = packetTeamClass.newInstance();
+
+			nameField.set(packetTeamObject, UUID.randomUUID().toString().substring(0, 15));
+			modeField.set(packetTeamObject, 0);
+			playersField.set(packetTeamObject, Arrays.asList(player.getName()));
+
+			changePacketCollisionType(packetTeamObject);
+			
+			if (!getSecurePlayers().contains(player)) {
+				MainCollision.getCollisionProtocol().sendPacket(player, packetTeamObject);
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	public static void changePacketCollisionType(Object packetTeamObject) throws Exception {
+		collisionRuleField.set(packetTeamObject, "never");
+	}
+
+	public static Class<?> getPacketTeamClass() {
+		return TeamUtils.packetTeamClass;
+	}
+
+	public static ArrayList<Player> getSecurePlayers() {
+		return TeamUtils.securePlayers;
+	}
+	
+}
